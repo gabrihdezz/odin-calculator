fc3475c795eaf1b4cca8599d3ee3b3c4fab4c349
Gabriel <123275045+gabrihdezz@users.noreply.github.com>
2025-01-12 02:17:41 +0000

    Add operations logic for the console

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            return "Operation not recognized"
+    }
+}
+
+console.log(operate('+', 1, 10));
